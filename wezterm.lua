f57d3cde99fd53473e66bf576a2c4413b876b46a
Ritesh Chepuri <riteshchepuri0@gmail.com>
2024-10-06 18:27:58 +0530

    feat: new formatter

--- wezterm.lua
+++ wezterm.lua
@@ -1,54 +1,46 @@
 local k = require("utils.keys")
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-
 local config = {
-    font_size = 12,
-    background = {
-        {
-            source = {
-                File = "C://Users//rites//Wallpapers/nebula.jpg"
-            },
-            horizontal_align = "Center"
-        }
-    },
-    enable_tab_bar = false,
-    window_decorations = "RESIZE",
-    default_domain = "WSL:Arch",
-    automatically_reload_config = true,
-    warn_about_missing_glyphs = false,
-    native_macos_fullscreen_mode = false,
-    color_scheme = "Panels (terminal.sexy)",
-    bold_brightens_ansi_colors = true,
-    debug_key_events = false,
-    window_close_confirmation = "NeverPrompt",
-    disable_default_mouse_bindings = true,
-    audible_bell = "Disabled",
-    default_cwd = "/mnt/c/Users/rites/",
-    font = wezterm.font "JetBrains Mono",
+	font_size = 11,
+	window_background_opacity = 0.8,
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	default_domain = "WSL:Arch",
+	automatically_reload_config = true,
+	warn_about_missing_glyphs = false,
+	native_macos_fullscreen_mode = false,
+	color_scheme = "OneDark (base16)",
+	bold_brightens_ansi_colors = true,
+	debug_key_events = false,
+	window_close_confirmation = "NeverPrompt",
+	disable_default_mouse_bindings = true,
+	audible_bell = "Disabled",
+	default_cwd = "/mnt/c/Users/rites/",
+	font = wezterm.font("JetBrains Mono"),
 
-    keys = {
-        k.alt_key(";", k.multiple_actions(":GoToFile")),
-        k.alt_key("q", k.multiple_actions("exit")),
-        k.alt_to_tmux_prefix("i", "c"),
-        k.alt_to_tmux_prefix("=", "%"),
-        k.alt_to_tmux_prefix("-", "-"),
-        k.alt_to_tmux_prefix("g", "g"),
-        k.alt_to_tmux_prefix("p", "T"),
-        k.alt_to_tmux_prefix("`", "n"),
-        k.alt_to_tmux_prefix("]", ")"),
-        k.alt_to_tmux_prefix("[", "("),
-        k.alt_to_tmux_prefix("u", "u"),
-        k.alt_to_tmux_prefix("y", "y"),
-        k.alt_to_tmux_prefix("i", "c"),
-        k.ctrl_to_tmux_prefix("1", "1"),
-        k.ctrl_to_tmux_prefix("2", "2"),
-        k.ctrl_to_tmux_prefix("3", "3"),
-    }
+	keys = {
+		k.alt_key(";", k.multiple_actions(":GoToFile")),
+		k.alt_key("q", k.multiple_actions("exit")),
+		k.alt_to_tmux_prefix("i", "c"),
+		k.alt_to_tmux_prefix("=", "%"),
+		k.alt_to_tmux_prefix("-", "-"),
+		k.alt_to_tmux_prefix("g", "g"),
+		k.alt_to_tmux_prefix("p", "T"),
+		k.alt_to_tmux_prefix("`", "n"),
+		k.alt_to_tmux_prefix("]", ")"),
+		k.alt_to_tmux_prefix("[", "("),
+		k.alt_to_tmux_prefix("u", "u"),
+		k.alt_to_tmux_prefix("y", "y"),
+		k.alt_to_tmux_prefix("i", "c"),
+		k.ctrl_to_tmux_prefix("1", "1"),
+		k.ctrl_to_tmux_prefix("2", "2"),
+		k.ctrl_to_tmux_prefix("3", "3"),
+	},
 }
 
 return config
